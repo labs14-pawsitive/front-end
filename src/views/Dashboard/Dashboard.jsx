175cd545aa8ac336919e1a18510beb1fa89162f2
Ming Liu <mliu@macpro.home>
2019-07-30 21:53:33 -0400

    dashboard status card, animal spotlight cards

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -20,6 +20,7 @@ import PropTypes from "prop-types";
 import ChartistGraph from "react-chartist";
 // react plugin for creating vector maps
 import { VectorMap } from "react-jvectormap";
+import axios from 'axios';
 
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
@@ -46,8 +47,6 @@ import Money from "@material-ui/icons/Money";
 import LibraryBooks from "@material-ui/icons/LibraryBooks";
 import Face from "@material-ui/icons/Face";
 
-
-
 // core components
 import GridContainer from "components/Grid/GridContainer.jsx";
 import GridItem from "components/Grid/GridItem.jsx";
@@ -66,113 +65,120 @@ import {
   dailySalesChart,
   emailsSubscriptionChart,
   completedTasksChart
-} from "variables/charts";
+} from "variables/shelterDashboardCharts.jsx";
 
-import dashboardStyle from "assets/jss/material-dashboard-pro-react/views/dashboardStyle";
+import shelterDashboardStyle from "assets/jss/material-dashboard-pro-react/views/shelterDashboardStyle";
 
 import priceImage1 from "assets/img/card-2.jpeg";
 import priceImage2 from "assets/img/card-3.jpeg";
 import priceImage3 from "assets/img/card-1.jpeg";
 
-/*
-const us_flag = require("assets/img/flags/US.png");
-const de_flag = require("assets/img/flags/DE.png");
-const au_flag = require("assets/img/flags/AU.png");
-const gb_flag = require("assets/img/flags/GB.png");
-const ro_flag = require("assets/img/flags/RO.png");
-const br_flag = require("assets/img/flags/BR.png");
-*/
-/*
-var mapData = {
-  AU: 760,
-  BR: 550,
-  CA: 120,
-  DE: 1300,
-  FR: 540,
-  GB: 690,
-  GE: 200,
-  IN: 200,
-  RO: 600,
-  RU: 300,
-  US: 2920
-};
-*/
-
-
-
 class Dashboard extends React.Component {
   state = {
-    value: 0,
-      labels: [],
-      series: []
+    //value: 0,
+    months: getFullMonths(),
+    //series: []
+    animal_3 : [],
+    animal_count : [],
+    application_30 : [],
+    donation_30 : [],
+    follower_count : [],
+    monthly_application : [],
+    monthly_donation : [],
+    recent_application : [],
+    shelter_info : []
+
   };
 
-  componentDidMount() {
-    const months = getFullMonths();
-    console.log(months)
-    this.setState({
-      labels : months,
-      series: [[50, 30, 50, 30, 50, 30, 50, 30, 50, 10, 100, 80]]
+  async componentDidMount() {
+    //const months = getFullMonths();
+    await axios
+    .get(`http://localhost:8000/api/dashboard/3`)
+    .then(results => {
+      this.setState({
+        animal_3 : results.data.animal_3,
+        animal_count : results.data.animal_count,
+        application_30 : results.data.application_30,
+        donation_30 : results.data.donation_30,
+        follower_count : results.data.follower_count,
+        monthly_application : results.data.monthly_application,
+        monthly_donation : results.data.monthly_donation,
+        recent_application : results.data.recent_application,
+        shelter_info : results.data.shelter_info
+      })
     })
-    //let result = getFullMonths();
-    //console.log(result)
-    console.log(this.state.labels)
+    .catch(error => {
+      console.log(error)
+    })
+    console.log(this.state)
   }
 
   handleChange = (event, value) => {
     this.setState({ value });
   };
+
   handleChangeIndex = index => {
     this.setState({ value: index });
   };
+
   render() {
     const { classes } = this.props;
-    const donationChartData = { labels : this.state.labels, series : this.state.series }
+    const donationChartData = { labels : this.state.months, series : this.state.series }
+    const customStyle = {
+      imgHover : {
+        maxHeight: "250px",
+        overflow: "hidden"
+      }
+    }
+    
+    if(!this.state.donation_30[0]) return <div>Still loading data</div>
+
     return (
+      
       <div>
         <GridContainer>
-          <GridItem xs={12} sm={6} md={6} lg={4}>
+          <GridItem xs={12} sm={6} md={6} lg={3}>
             <Card>
               <CardHeader color="warning" stats icon>
                 <CardIcon color="warning">
                   <Pets />
                 </CardIcon>
                 <p className={classes.cardCategory}>Current Animals</p>
-                <h3 className={classes.cardTitle}>
-                  13 <small>Dogs</small> {" / "} 25 <small>Cats</small>
-                </h3>
+                
+                  {this.state.animal_count.map((count, key) => (
+                    <h3 className={classes.cardTitle}>{count.count}{' '}{count.species}{count.count > 1 ? "s" : ""}</h3>
+                  ))}
+                
                 
               </CardHeader>
               <CardFooter stats>
                 <div className={classes.stats}>
-                  <Danger>
-                    <Warning />
-                  </Danger>
-                  <a href="#pablo" onClick={e => e.preventDefault()}>
-                    That's a lot of animals
-                  </a>
+                 <Pets />
+                  
+                    Current animals available for adoptions
+                 
                 </div>
               </CardFooter>
             </Card>
           </GridItem>
-          <GridItem xs={12} sm={6} md={6} lg={4}>
+          <GridItem xs={12} sm={6} md={6} lg={3}>
             <Card>
               <CardHeader color="success" stats icon>
                 <CardIcon color="success">
                  <Money />
                 </CardIcon>
                 <p className={classes.cardCategory}>Recent Donations</p>
-                <h3 className={classes.cardTitle}>$300</h3>
+                <h3 className={classes.cardTitle}>${this.state.donation_30[0].total}</h3>
               </CardHeader>
               <CardFooter stats>
                 <div className={classes.stats}>
                   <DateRange />
-                  Updated in the Last 24 Hours
+                  Total donations from the past 30 days
                 </div>
               </CardFooter>
             </Card>
           </GridItem>
-          {/* 
+          
           <GridItem xs={12} sm={6} md={6} lg={3}>
             <Card>
               <CardHeader color="danger" stats icon>
@@ -180,30 +186,30 @@ class Dashboard extends React.Component {
                   <LibraryBooks />
                 </CardIcon>
                 <p className={classes.cardCategory}>Recent Applications</p>
-                <h3 className={classes.cardTitle}>5</h3>
+                <h3 className={classes.cardTitle}>{this.state.application_30[0].count}</h3>
               </CardHeader>
               <CardFooter stats>
                 <div className={classes.stats}>
                   <LocalOffer />
-                  Updated within last 24 Hours
+                  Total application from the past 30 days
                 </div>
               </CardFooter>
             </Card>
           </GridItem>
-          */}
-          <GridItem xs={12} sm={6} md={6} lg={4}>
+          
+          <GridItem xs={12} sm={6} md={6} lg={3}>
             <Card>
               <CardHeader color="info" stats icon>
                 <CardIcon color="info">
                   <Face />
                 </CardIcon>
                 <p className={classes.cardCategory}>Followers</p>
-                <h3 className={classes.cardTitle}>35</h3>
+                <h3 className={classes.cardTitle}>{this.state.follower_count[0].count}</h3>
               </CardHeader>
               <CardFooter stats>
                 <div className={classes.stats}>
                   <Update />
-                  Just Updated
+                  Total Pawsnfind members who are following you
                 </div>
               </CardFooter>
             </Card>
@@ -447,14 +453,15 @@ class Dashboard extends React.Component {
             </Card>
           </GridItem>
         </GridContainer>
-        <h3>Manage Listings</h3>
+        <h3>Animal Spotlights</h3>
         <br />
         <GridContainer>
-          <GridItem xs={12} sm={12} md={4}>
+          {this.state.animal_3.map((animal, key) => (
+            <GridItem xs={12} sm={12} md={4}>
             <Card product className={classes.cardHover}>
-              <CardHeader image className={classes.cardHeaderHover}>
+              <CardHeader image className={classes.cardHeaderHover} style={customStyle.imgHover}>
                 <a href="#pablo" onClick={e => e.preventDefault()}>
-                  <img src={priceImage1} alt="..." />
+                  <img src={animal.img_url} alt={animal.name} />
                 </a>
               </CardHeader>
               <CardBody>
@@ -492,15 +499,18 @@ class Dashboard extends React.Component {
                 </div>
                 <h4 className={classes.cardProductTitle}>
                   <a href="#pablo" onClick={e => e.preventDefault()}>
-                    Cozy 5 Stars Apartment
+                    {animal.name}
                   </a>
                 </h4>
-                <p className={classes.cardProductDesciprion}>
+               {/*
+                 <p className={classes.cardProductDesciprion}>
                   The place is close to Barceloneta Beach and bus stop just 2
                   min by walk and near to {'"'}Naviglio{'"'} where you can enjoy
                   the main night life in Barcelona.
                 </p>
+                */}
               </CardBody>
+              {/*
               <CardFooter product>
                 <div className={classes.price}>
                   <h4>$899/night</h4>
@@ -509,8 +519,15 @@ class Dashboard extends React.Component {
                   <Place /> Barcelona, Spain
                 </div>
               </CardFooter>
+              */}
             </Card>
           </GridItem>
+          ))}
+          
+
+
+
+          {/* 
           <GridItem xs={12} sm={12} md={4}>
             <Card product className={classes.cardHover}>
               <CardHeader image className={classes.cardHeaderHover}>
@@ -632,7 +649,7 @@ class Dashboard extends React.Component {
                 </div>
               </CardFooter>
             </Card>
-          </GridItem>
+          </GridItem>*/}
         </GridContainer>
       </div>
     );
@@ -643,4 +660,4 @@ Dashboard.propTypes = {
   classes: PropTypes.object.isRequired
 };
 
-export default withStyles(dashboardStyle)(Dashboard);
+export default withStyles(shelterDashboardStyle)(Dashboard);
