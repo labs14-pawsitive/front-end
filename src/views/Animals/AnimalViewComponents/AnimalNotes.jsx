42bb80db6b89ed2c9379478c5c044105b6bbfa99
Ming Liu <mliu@macpro.home>
2019-08-04 08:57:31 -0400

    modify url link to include local host root

--- src/views/Animals/AnimalViewComponents/AnimalNotes.jsx
+++ src/views/Animals/AnimalViewComponents/AnimalNotes.jsx
@@ -146,7 +146,7 @@ class AnimalNotes extends React.Component {
             <GridItem xs={12} sm={12} md={12} lg={4}>
                 <Card style={this.state.urlClicked? customStyle.urlClicked : customStyle.urlButton}>
                     <div onClick={this.handleUrlClick} >
-                        {this.state.urlClicked ? `http${window.location.hostname.indexOf(".com") !== -1 ? "s" : ""}://${window.location.hostname}/application/${this.props.shelter_id}/${this.props.animal_id}`: "Generate Application Link"}  
+                        {this.state.urlClicked ? `http${window.location.hostname.indexOf(".com") !== -1 ? "s" : ""}://${window.location.host}/application/${this.props.shelter_id}/${this.props.animal_id}`: "Generate Application Link"}  
                     </div>
                 </Card>
                 <Card style={customStyle.textFieldNote}>
