acd80218b307a59901527302778561b1e4b9b672
jimpakdev <jamesjiminpak@gmail.com>
2019-08-04 23:21:53 -0700

    breakpoints adjusted and cleanup

--- src/views/ShelterProfile/Contacts.jsx
+++ src/views/ShelterProfile/Contacts.jsx
@@ -83,7 +83,13 @@ changeHandler = e => {
       
 cancelClick = e => {
   e.preventDefault()
-  this.setState({editMode : !this.state.editMode})
+  this.setState({
+    editMode : !this.state.editMode,
+    contact: this.props.contact,
+    nameState: 'success',
+    emailState: 'success',
+    phoneState: 'success',
+  })
 }
 
 deleteContact = e => {
