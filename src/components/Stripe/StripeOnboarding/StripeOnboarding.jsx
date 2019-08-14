4176f6c0319370bf19f8815239b7dbc6f75e8852
jimpakdev <jamesjiminpak@gmail.com>
2019-08-14 10:25:55 -0700

    stripe onboarding form creates valid account

--- src/components/Stripe/StripeOnboarding/StripeOnboarding.jsx
+++ src/components/Stripe/StripeOnboarding/StripeOnboarding.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
 import FormControl from "@material-ui/core/FormControl";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
@@ -19,6 +19,7 @@ import CardBody from "components/Card/CardBody.jsx";
 import CardIcon from "components/Card/CardIcon.jsx";
 import CardHeader from "components/Card/CardHeader.jsx";
 import CustomInput from "components/CustomInput/CustomInput.jsx";
+import ImageUpload from "components/CustomUpload/ImageUpload.jsx";
 
 // custom classes
 import regularFormsStyle from "assets/jss/material-dashboard-pro-react/views/regularFormsStyle";
@@ -31,20 +32,16 @@ function TextMaskCustom(props) {
     const { inputRef, ...other } = props;
 
     return (
-        <MaskedInput 
-            { ...other } 
+        <MaskedInput
+            {...other}
             ref={ref => {
-                inputRef( ref ? ref.inputElement : null )
+                inputRef(ref ? ref.inputElement : null)
             }}
             mask={['(', /\d/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
             placeholderChar={'\u2000'}
         />
     )
 }
-
-TextMaskCustom.propTypes = {
-    inputRef: PropTypes.func.isRequired,
-};
 
 class StripeOnboarding extends React.Component {
     constructor(props) {
@@ -66,133 +63,192 @@ class StripeOnboarding extends React.Component {
             dob_day: '',
             dob_month: '',
             dob_year: '',
-            file_front: '',
-            file_back: '',
             bankToken: {},
-            inputField: '',
-            shelter: {},
-        }
-
-    };
-
-    componentDidMount() {
-
-        axios
-            .get(`${process.env.REACT_APP_BACKEND_URL}/api/shelters/${localStorage.getItem('shelter_id')}`)
-            .then(shelter => {
-                console.log(shelter.data)
-                this.setState({
-                    shelter: shelter.data
-                })
-                console.log(this.state.shelter)
-            })
-            .catch(error => {
-                console.log(error)
-            })
-
+            frontImage: null,
+            backImage: null,
+            inputField: "",
+            shelter: {}
+        };
     }
 
-    testEditMode = event => {
-
-        event.preventDefault();
-
-        this.setState({
-            editMode: !this.state.editMode
-        })
-    };
+    componentDidMount() {
+        axios
+            .get(
+                `${
+                process.env.REACT_APP_BACKEND_URL
+                }/api/shelters/${localStorage.getItem("shelter_id")}`
+            )
+            .then(shelter => {
+                console.log(shelter.data);
+                this.setState({
+                    shelter: shelter.data
+                });
+                console.log(this.state.shelter);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
 
     handleInput = event => {
 
         this.setState({
             ...this.state,
             [event.target.id]: event.target.value
-        })
+        });
     };
 
-    handleSubmit = async (e) => {
+    handleSubmit = async (event) => {
 
         const stripe = window.Stripe("pk_test_x16KAsU777aRjmWMukoNMKfG00PisbA3Vh");
 
         const state = this.state;
 
-        let { token } = stripe.createToken('bank_account', {
-            country: 'US',
-            currency: 'usd',
-            routing_number: state.routing_number,
-            account_number: state.account_number,
-            account_holder_name: `${state.first_name}` + `${state.last_name}`,
-            account_holder_type: 'company',
-        }).then(async result => {
-            // Handle result.error or result.token
-            console.log(result.token)
-            await axios
-                .post(`http://127.0.0.1:8000/api/stripe/account`,
-                    {
-                        first_name: state.first_name,
-                        last_name: state.last_name,
-                        routing_number: state.routing_number,
-                        account_number: state.account_number,
-                        shelterID: localStorage.getItem('shelter_id'),
-                        address1: state.address_1,
-                        address2: state.address_2,
-                        city: state.city,
-                        state: state.state,
-                        zip: state.zip,
-                        email: state.email,
-                        phone: state.phone_number,
-                        ssn_last_4: state.ssn_last_4,
-                        dob_day: state.dob_day,
-                        dob_month: state.dob_month,
-                        dob_year: state.dob_year,
-                        bankToken: result.token.id
-                    })
-                .then(results => {
-                    console.log(results)
-                })
-                .catch(error => {
-                    console.log(error)
-                })
-        })
-            .catch(error => {
-                console.log(error)
+        let frontImage = null;
+        if (this.state.frontImage) {
+            frontImage = new FormData();
+            frontImage.append("file", this.state.frontImage);
+            frontImage.append("purpose", "identity_document");
+        }
+        let backImage = null;
+        if (this.state.backImage) {
+            backImage = new FormData();
+            backImage.append("file", this.state.backImage);
+            backImage.append("purpose", "identity_document");
+        }
+        let frontData = null;
+        const frontResult = await axios
+            .post("https://uploads.stripe.com/v1/files", frontImage, {
+                headers: {
+                    Authorization: `Bearer pk_test_x16KAsU777aRjmWMukoNMKfG00PisbA3Vh`
+                }
             })
+            .then(result => {
+                frontData = result.data.id;
+                console.log(result.data.id);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+
+        let backData = null;
+        const backResult = await axios
+            .post("https://uploads.stripe.com/v1/files", backImage, {
+                headers: {
+                    Authorization: `Bearer pk_test_x16KAsU777aRjmWMukoNMKfG00PisbA3Vh`
+                }
+            })
+            .then(result => {
+                backData = result.data.id;
+                console.log(result.data.id);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+
+        const header = {
+            headers: {
+                "Content-Type": "multipart/form-data"
+            }
+        };
+        let { token } = stripe
+            .createToken("bank_account", {
+                country: "US",
+                currency: "usd",
+                routing_number: "110000000",
+                account_number: "000123456789",
+                account_holder_name: `${state.first_name}` + `${state.last_name}`,
+                account_holder_type: "company"
+            })
+            .then(async result => {
+                // Handle result.error or result.token
+                console.log(result.token);
+                await axios
+                    .post(
+                        `${process.env.REACT_APP_BACKEND_URL}/api/stripe/account`,
+                        {
+                            first_name: state.first_name,
+                            last_name: state.last_name,
+                            routing_number: state.routing_number,
+                            account_number: state.account_number,
+                            shelterID: localStorage.getItem("shelter_id"),
+                            address1: state.address_1,
+                            address2: state.address_2,
+                            city: state.city,
+                            state: state.state,
+                            zip: state.zip,
+                            email: state.email,
+                            phone: state.phone_number,
+                            ssn_last_4: state.ssn_last_4,
+                            dob_day: state.dob_day,
+                            dob_month: state.dob_month,
+                            dob_year: state.dob_year,
+                            bankToken: result.token.id,
+                            frontImage: frontData,
+                            backImage: backData,
+
+                        }
+                    )
+                    // RETURNS NEWLY CREATED STRIPE ACCOUNT ID
+                    .then(async result => {
+                        console.log(result);
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+            })
+            .catch(error => {
+                console.log(error);
+            });
     };
 
+    frontImage = image => {
+        this.setState({ frontImage: image });
+    };
+
+    backImage = image => {
+        this.setState({ backImage: image });
+    };
 
     render() {
 
-        const { classes } = this.props
+        const { classes } = this.props;
 
         const customStyle = {
             formStyle: {
-                display: 'flex',
-                flexDirection: 'column',
-                justify: 'center',
-                alignItems: 'center',
+                display: "flex",
+                flexDirection: "column",
+                justify: "center",
+                alignItems: "center"
             },
             headerStyle: {
-                fontSize: '30px'
+                fontSize: "30px"
             },
             submitButtonStyle: {
-                width: '200px',
-                height: '40px',
-                marginRight: '20px',
+                width: "200px",
+                height: "40px",
+                marginRight: "20px"
             },
             fieldAlert: {
                 color: "#333333 !important",
-            }
+            },
 
-        }
-
+        };
         return (
             <>
-                <FormControl >
+
+                <FormControl>
+
                     <Card style={{ width: "600px" }}>
+
                         <CardHeader color="primary" icon>
                             <CardIcon color="primary">
                                 <Assignment />
                             </CardIcon>
-                            <h4 className={classes.cardIconTitle}> Create Stripe Account </h4>
+                            <h4 className={classes.cardIconTitle}>
+                                {" "}
+                                Create Stripe Account{" "}
+                            </h4>
                         </CardHeader>
 
                         <CardBody>
@@ -328,17 +384,17 @@ class StripeOnboarding extends React.Component {
                                 <GridItem xs={6} sm={6} md={6} lg={6} xl={6} >
                                     <CustomInput
                                         labelText="Phone Number"
-                                        id="phone_number"
+                                        id={this.state.editMode ? "phone_number_disabled" : "phone_number"}
                                         formControlProps={{
                                             fullWidth: true
                                         }}
                                         inputProps={{
                                             type: "text",
-                                            onChange: this.handleInput,
                                             value: this.state.phone_number,
+                                            onChange: (event) => this.handleInput(event),
                                             inputComponent: TextMaskCustom,
                                         }}
-                                        style={ this.state.editMode ? "" : customStyle.fieldAlert }
+                                        style={this.state.editMode ? "" : customStyle.fieldAlert}
                                     />
                                 </GridItem>
                             </GridContainer>
@@ -437,6 +493,25 @@ class StripeOnboarding extends React.Component {
                                 </GridItem>
                             </GridContainer>
 
+                            <GridContainer justify="center">
+                                <GridItem>
+                                    <Typography>
+                                        <h4 className={classes.cardIconTitle}>
+                                            Front ID
+                                                </h4>
+                                    </Typography>
+                                    <ImageUpload callback={this.frontImage} />
+                                </GridItem>
+
+                                <GridItem>
+                                    <Typography>
+                                        <h4 className={classes.cardIconTitle}>
+                                            Back ID
+                                                </h4>
+                                    </Typography>
+                                    <ImageUpload callback={this.backImage} />
+                                </GridItem>
+                            </GridContainer>
 
                             <GridContainer justify="flex-end" >
                                 <Button
@@ -451,13 +526,11 @@ class StripeOnboarding extends React.Component {
 
                         </CardBody>
                     </Card>
-
                 </FormControl>
+
             </>
-        )
+        );
     }
-
-};
-
+}
 export default withStyles(regularFormsStyle)(StripeOnboarding);
 
