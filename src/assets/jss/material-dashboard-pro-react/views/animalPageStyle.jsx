683f31a86e1b2f80886bdacd998bbf1f49ec2280
Ming Liu <33786625+skysthelimit68@users.noreply.github.com>
2019-08-28 13:34:27 -0400

    Update animalPageStyle.jsx

--- src/assets/jss/material-dashboard-pro-react/views/animalPageStyle.jsx
+++ src/assets/jss/material-dashboard-pro-react/views/animalPageStyle.jsx
@@ -124,6 +124,19 @@ addressStyle: {
     minWidth: "140px",
     marginBottom: "-10px",
 },
+  animalNameStyle: {
+    wordWrap: "break-word", 
+    lineHeight:"1", 
+    fontFamily: "Coiny, cursive" ,
+    color: "white", 
+    fontWeight: "bold", 
+    fontSize: "4rem", 
+    textShadow: "5px 5px #2b2b2b61",
+    textTransform: "uppercase",
+    "@media (max-width: 576px)": {
+    fontSize: "3.5rem"
+    },
+  },
 ...buttonStyle,
   
 
