410ddf13b76d5abb426e8a5098b580cb8f4368a4
cmr629 <37821010+cmr629@users.noreply.github.com>
2019-08-27 20:12:34 -0400

    Update AddAnimalForm.jsx

--- src/views/Animals/AddAnimalForm.jsx
+++ src/views/Animals/AddAnimalForm.jsx
@@ -12,7 +12,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import {addAnimal, fetchOptions} from '../../actions/animalAction';
-import ImageUpload from '../../components/ImageUpload/ImageUploadEdit'
+import ImageUpload from '../../components/ImageUpload/ImageUpload'
 import { axiosWithAuth } from 'axiosWithAuth';
 
 // @material-ui/core components
